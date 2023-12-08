765d4c37499bfa40799bc198c1963611640813f6
haquocdat543 <wwwdatha543@gmail.com>
2023-12-08 09:34:32 +0000

    Update main.tf rds

--- rds/main.tf
+++ rds/main.tf
@@ -47,8 +47,8 @@ resource "aws_subnet" "RdsSubnet1" {
 }
 resource "aws_subnet" "RdsSubnet2" {
   vpc_id            = aws_vpc.prod-vpc.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1d"
 
   tags = {
     Name = "RdsSubnet2"

26ee69e814c75082e1f656811f31e38c4c323c77
haquocdat543 <wwwdatha543@gmail.com>
2023-12-08 10:00:42 +0000

    Update main.tf rds

--- rds/main.tf
+++ rds/main.tf
@@ -86,6 +86,7 @@ resource "aws_db_instance" "example" {
   password             = "haquocdat"
   parameter_group_name = "default.mysql5.7"
   db_subnet_group_name = aws_db_subnet_group.example.name
+  skip_final_snapshot = true
   tags = {
     Name = "Rds"
     Env = "Dev"

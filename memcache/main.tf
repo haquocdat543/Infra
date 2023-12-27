2c309622393b28246397766f1704b2c1515c0dc6
haquocdat543 <wwwdatha543@gmail.com>
2023-12-27 04:02:49 +0000

    Add memcache

---
+++ memcache/main.tf
@@ -0,0 +1,15 @@
+# Profile configuration
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = var.profile
+}
+
+resource "aws_elasticache_cluster" "example" {
+  cluster_id           = "haquocdat-memcache"
+  engine               = "memcached"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 2
+  parameter_group_name = "default.memcached1.6"
+  port                 = 11211
+}

282ebde4e98284ab99b23943bde06a90e43014d0
haquocdat543 <wwwdatha543@gmail.com>
2023-12-27 03:44:38 +0000

    Add redis

---
+++ redis/main.tf
@@ -0,0 +1,16 @@
+# Profile configuration
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = var.profile
+}
+
+resource "aws_elasticache_cluster" "example" {
+  cluster_id           = "haquocdat-redis"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  engine_version       = "7.1"
+  port                 = 6379
+}

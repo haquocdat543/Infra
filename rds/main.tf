309ee856e1b6e068f9fe42520c6cc54ab52284d0
haquocdat543 <wwwdatha543@gmail.com>
2024-01-07 09:00:05 +0000

    Update rds engine and version
    Update rds engine

--- rds/main.tf
+++ rds/main.tf
@@ -104,12 +104,12 @@ resource "aws_security_group" "Rds_SG" {
 resource "aws_db_instance" "example" {
   allocated_storage    = 20
   storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
+  engine               = "mariadb"
+  engine_version       = "10.11"
   instance_class       = "db.t2.micro"
   username             = "haquocdat"
   password             = "haquocdat"
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mariadb10.11"
   db_subnet_group_name = aws_db_subnet_group.example.name
   vpc_security_group_ids = [ aws_security_group.Rds_SG.id ]
   skip_final_snapshot = true

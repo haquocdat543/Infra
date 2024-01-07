06fc4a771298aa1b753ff285e7fb3c7a2b2c6836
haquocdat543 <wwwdatha543@gmail.com>
2024-01-07 11:23:15 +0000

    Modified rda access public

--- rds/main.tf
+++ rds/main.tf
@@ -110,6 +110,7 @@ resource "aws_db_instance" "example" {
   username             = "haquocdat"
   password             = "haquocdat"
   port                 = 3306
+  publicly_accessible  = true
   parameter_group_name = "default.mariadb10.6"
   db_subnet_group_name = aws_db_subnet_group.example.name
   vpc_security_group_ids = [ aws_security_group.Rds_SG.id ]

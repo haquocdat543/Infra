55bbbcc1d5b7140b3f8ec021fddc3641f255e1c6
haquocdat543 <wwwdatha543@gmail.com>
2024-01-07 12:20:44 +0000

    Update rds vpc attributes

--- rds/main.tf
+++ rds/main.tf
@@ -7,6 +7,8 @@ provider "aws" {
 
 # Create VPC
 resource "aws_vpc" "prod-vpc" {
+  enable_dns_support = true
+  enable_dns_hostnames = true
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "RDS"

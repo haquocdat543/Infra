5f7671dcc25250096844a2b6687ae828cd37d843
haquocdat543 <wwwdatha543@gmail.com>
2024-01-08 00:20:29 +0000

    Update network dms

--- dms/main.tf
+++ dms/main.tf
@@ -5,6 +5,103 @@ provider "aws" {
   profile                  = var.profile
 }
 
+# Create VPC
+resource "aws_vpc" "prod-vpc" {
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "RDS"
+    Env = "Dev"
+  }
+}
+# Create Internet Gateway
+resource "aws_internet_gateway" "prod_gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+  tags = {
+    Name = "RDSInternetGateWay"
+    Env = "Dev"
+  }
+}
+# Create Custom Route Table
+resource "aws_route_table" "RDSRouteTable" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod_gw.id
+  }
+
+  tags = {
+    Name = "RDS_RouteTable"
+  }
+}
+# Create a Subnet
+resource "aws_subnet" "DMSSubnet1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "DMSSubnet1"
+    Env = "Dev"
+  }
+}
+resource "aws_subnet" "DMSSubnet2" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1d"
+
+  tags = {
+    Name = "DMSSubnet2"
+    Env = "Dev"
+  }
+}
+# Create Associate Subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.DMSSubnet1.id
+  route_table_id = aws_route_table.RDSRouteTable.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.DMSSubnet2.id
+  route_table_id = aws_route_table.RDSRouteTable.id
+}
+
+resource "aws_db_subnet_group" "example" {
+  name       = "main"
+
+  subnet_ids = [ aws_subnet.DMSSubnet1.id, aws_subnet.DMSSubnet2.id ]
+
+  tags = {
+    Name = "RDS Subnet name"
+  }
+}
+
+resource "aws_security_group" "DMS_SG" {
+  name        = "DMS-SG"
+  description = "Allow 3306"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "DMS-SG"
+  }
+}
 # Database Migration Service requires the below IAM Roles to be created before
 # replication instances can be created. See the DMS Documentation for
 # additional information: https://docs.aws.amazon.com/dms/latest/userguide/security-iam.html#CHAP_Security.APIRole
@@ -66,7 +163,7 @@ resource "aws_dms_replication_instance" "test" {
   publicly_accessible          = true
   replication_instance_class   = "dms.t2.micro"
   replication_instance_id      = "test-dms-replication-instance-tf"
-  replication_subnet_group_id  = aws_dms_replication_subnet_group.test-dms-replication-subnet-group-tf.id
+  replication_subnet_group_id  = aws_db_subnet_group.example.id
 
   tags = {
     Name = "test"

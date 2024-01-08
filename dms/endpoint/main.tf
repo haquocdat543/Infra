c141d594065826c9e03c1493f656518ad1626c5e
haquocdat543 <wwwdatha543@gmail.com>
2024-01-08 04:39:35 +0000

    Add dms endpoint

---
+++ dms/endpoint/main.tf
@@ -0,0 +1,46 @@
+# Profile configuration
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = var.profile
+}
+
+# Create a new endpoint
+resource "aws_dms_endpoint" "source" {
+  database_name               = "test"
+  endpoint_id                 = "test-dms-source-endpoint-tf"
+  endpoint_type               = "source"
+  engine_name                 = "aurora"
+  extra_connection_attributes = ""
+  username                    = "haquocdat"
+  password                    = "haquocdat"
+  port                        = 3306
+  server_name                 = "test"
+  ssl_mode                    = "none"
+
+  tags = {
+    Name = "Source"
+    Env  = "Dev"
+  }
+
+}
+
+# Create a new endpoint
+resource "aws_dms_endpoint" "target" {
+  database_name               = "test"
+  endpoint_id                 = "test-dms-target-endpoint-tf"
+  endpoint_type               = "target"
+  engine_name                 = "aurora"
+  extra_connection_attributes = ""
+  username                    = "haquocdat"
+  password                    = "haquocdat"
+  port                        = 3306
+  server_name                 = "test"
+  ssl_mode                    = "none"
+
+  tags = {
+    Name = "Target"
+    Env  = "Dev"
+  }
+
+}

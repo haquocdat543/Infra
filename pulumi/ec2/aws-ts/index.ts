56bd474e38469ce4bb3f4dfb67bed8ed362bec74
haquocdat543 <wwwdatha543@gmail.com>
2024-01-10 15:18:09 +0000

    Add pulumi

---
+++ pulumi/ec2/aws-ts/index.ts
@@ -0,0 +1,31 @@
+// Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
+
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+
+// Get the id for the latest Amazon Linux AMI
+const ami = "ami-0dfa284c9d7b2adad"
+
+// create a new security group for port 80
+const group = new aws.ec2.SecurityGroup("web-secgrp", {
+    ingress: [
+        { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+    ],
+});
+
+// (optional) create a simple web server using the startup script for the instance
+const userData =
+`#!/bin/bash
+echo "Hello, World!" > index.html
+nohup python -m SimpleHTTPServer 80 &`;
+
+const server = new aws.ec2.Instance("web-server-www", {
+    tags: { "Name": "web-server-www" },
+    instanceType: aws.ec2.InstanceType.T2_Micro, // t2.micro is available in the AWS free tier
+    vpcSecurityGroupIds: [ group.id ], // reference the group object above
+    ami: ami,
+    userData: userData,              // start a simple web server
+});
+
+export const publicIp = server.publicIp;
+export const publicHostName = server.publicDns;

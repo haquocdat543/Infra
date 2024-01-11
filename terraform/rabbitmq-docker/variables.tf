57d46d65fbcfcf6b52b0cff0d426a34ec9c36774
haquocdat543 <wwwdatha543@gmail.com>
2024-01-11 10:58:25 +0000

    Add rabbitmq docker terrafoem

---
+++ terraform/rabbitmq-docker/variables.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+  default = "default"
+  type = string
+}
+
+variable "ami_id" {
+  default = "ami-07c589821f2b353aa"
+  type = string
+}
+
+variable "key_pair" {
+  type = string
+}
+
+

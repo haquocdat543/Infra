10365f6e2ef3ccff0ab121442fdbebd91f8afa67
haquocdat543 <wwwdatha543@gmail.com>
2024-01-23 12:41:38 +0000

    Add network gcp

---
+++ gcp/terraform/network/main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  project     = var.projectId
+  region      = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "vpc-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.64.0/22"
+  }
+}

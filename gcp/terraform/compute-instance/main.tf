3279a9ce21f04a1abe18146f9cc8bf45514cc032
haquocdat543 <wwwdatha543@gmail.com>
2024-01-23 14:09:54 +0000

    Update gcp compute instance

--- gcp/terraform/compute-instance/main.tf
+++ gcp/terraform/compute-instance/main.tf
@@ -8,22 +8,6 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = true
 }
 
-resource "google_compute_subnetwork" "vpc_subnetwork" {
-  name          = "test-subnetwork"
-  ip_cidr_range = "10.0.0.0/16"
-  region        = var.region
-  network       = google_compute_network.vpc_network.id
-  secondary_ip_range {
-    range_name    = "services-range"
-    ip_cidr_range = "192.168.1.0/24"
-  }
-
-  secondary_ip_range {
-    range_name    = "pod-ranges"
-    ip_cidr_range = "192.168.64.0/22"
-  }
-}
-
 resource "google_service_account" "test_sa" {
   account_id   = "my-custom-sa"
   display_name = "Custom SA for VM Instance"

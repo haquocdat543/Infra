6a7c98de1f2fcef5f4637a24a2f68a3b92d84a81
haquocdat543 <wwwdatha543@gmail.com>
2024-01-25 01:42:13 +0000

    Duplicate gcp compute instance

--- gcp/terraform/compute-instance/main.tf
+++ gcp/terraform/compute-instance/main.tf
@@ -26,8 +26,8 @@ resource "google_service_account" "test_sa" {
   display_name = "Custom SA for VM Instance"
 }
 
-resource "google_compute_instance" "test_instance" {
-  name         = "test-instance"
+resource "google_compute_instance" "master" {
+  name         = "master-instance"
   machine_type = "n2-standard-2"
   zone         = "asia-northeast1-a"
 
@@ -65,13 +65,62 @@ resource "google_compute_instance" "test_instance" {
   }
 }
 
-output "internal-ip" {
-  description = "Private IP"
-  value       = google_compute_instance.test_instance.network_interface.0.network_ip
+resource "google_compute_instance" "worker" {
+  name         = "worker-instance"
+  machine_type = "n2-standard-2"
+  zone         = "asia-northeast1-a"
+
+  tags = ["http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {}
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.test_sa.email
+    scopes = ["cloud-platform"]
+  }
 }
 
-output "external-ip" {
-  description = "Public IP"
-  value       = google_compute_instance.test_instance.network_interface.0.access_config.0.nat_ip
+output "master-internal-ip" {
+  description = "Master private IP"
+  value       = google_compute_instance.master.network_interface.0.network_ip
+}
+
+output "master-external-ip" {
+  description = "Master public IP"
+  value       = google_compute_instance.master.network_interface.0.access_config.0.nat_ip
+}
+
+output "worker-internal-ip" {
+  description = "Worker private IP"
+  value       = google_compute_instance.worker.network_interface.0.network_ip
+}
+
+output "worker-external-ip" {
+  description = "Worker public IP"
+  value       = google_compute_instance.worker.network_interface.0.access_config.0.nat_ip
 }
 

26e53aba987912d4253a70a1e866f8ef4fceac6a
haquocdat543 <wwwdatha543@gmail.com>
2024-01-27 03:17:25 +0000

    Update gke gcp

--- gcp/terraform/gke/main.tf
+++ gcp/terraform/gke/main.tf
@@ -8,20 +8,30 @@ resource "google_service_account" "default" {
   display_name = "Service Account"
 }
 
-resource "google_container_cluster" "my_cluster" {
+resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = var.zone
-
+  deletion_protection = false
   remove_default_node_pool = true
-
-  node_pool {
-    name       = "custom-pool"
-    initial_node_count = 3
-    machine_type       = "n1-standard-2"
-    disk_size_gb       = 50
-  }
-
+  initial_node_count = 1
   network_policy {
     enabled = true
   }
 }	
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb       = 50
+
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}

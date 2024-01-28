9ebef652956399cd07d8206dae700fddcf3eced6
haquocdat543 <wwwdatha543@gmail.com>
2024-01-28 07:48:55 +0000

    Add azure tertaform disk

---
+++ azure/disk/main.tf
@@ -0,0 +1,23 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East Asia"
+}
+
+resource "azurerm_managed_disk" "source" {
+  name                 = "hqddisk"
+  location             = azurerm_resource_group.example.location
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "20"
+
+  tags = {
+    environment = "staging"
+  }
+}

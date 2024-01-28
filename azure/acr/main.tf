d9a199387b56134f55507a290e0fee9bcd6d5f69
haquocdat543 <wwwdatha543@gmail.com>
2024-01-28 06:47:07 +0000

    Add acr

---
+++ azure/acr/main.tf
@@ -0,0 +1,18 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East Asia"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "hqdAcr"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Premium"
+  admin_enabled       = false
+}

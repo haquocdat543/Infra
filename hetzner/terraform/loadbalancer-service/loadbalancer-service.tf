2e799dbeebc0b651ce47666a7330d9d04003cd5d
Ha Dat <hadat@Has-MacBook-Pro.local>
2024-02-11 15:26:32 +0700

    Add hetzner terraform loadbalancer service

---
+++ hetzner/terraform/loadbalancer-service/loadbalancer-service.tf
@@ -0,0 +1,24 @@
+resource "hcloud_load_balancer_service" "load_balancer_service" {
+  load_balancer_id = hcloud_load_balancer.load_balancer.id
+  protocol         = "http"
+
+  http {
+    sticky_sessions = true
+    cookie_name     = "EXAMPLE_STICKY"
+  }
+
+  health_check {
+    protocol = "http"
+    port     = 80
+    interval = 10
+    timeout  = 5
+
+    http {
+      domain       = "example.com"
+      path         = "/healthz"
+      response     = "OK"
+      tls          = true
+      status_codes = ["200"]
+    }
+  }
+}

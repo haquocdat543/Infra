0b322a72d8e117d60dd3b9e8d2cdc51aea52852c
haquocdat543 <wwwdatha543@gmail.com>
2024-02-20 00:58:04 +0700

    Update gcp terraform swarm ansible ( update ansible.sh )

--- gcp/terraform/docker-swarm-ansible/ansible.sh
+++ gcp/terraform/docker-swarm-ansible/ansible.sh
@@ -4,3 +4,4 @@ ansible-playbook ./playbook_install_docker.yaml -i inventory
 ansible-playbook ./playbook_init_swarm.yaml -i inventory
 ansible-playbook ./playbook_add_manager.yaml -i inventory
 ansible-playbook ./playbook_add_worker.yaml -i inventory
+ansible-playbook ./playbook_get_nodes.yaml -i inventory

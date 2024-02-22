fb9c7e0d3b718a7fc4b66704fc03ffabba7be248
haquocdat543 <wwwdatha543@gmail.com>
2024-02-22 14:01:50 +0700

    Update azure terraform acr ( seperation )

--- azure/acr/container_registry.tf
+++ azure/acr/container_registry.tf
@@ -1,14 +1,3 @@
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
-
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East Asia"
-}
-
 resource "azurerm_container_registry" "acr" {
   name                = "hqdAcr"
   resource_group_name = azurerm_resource_group.example.name

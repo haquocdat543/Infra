c85c5f01089ef8f4aa46fadffdc78ce138c9f5e1
haquocdat543 <wwwdatha543@gmail.com>
2024-02-22 11:36:30 +0700

    Add gcp terraform project

---
+++ gcp/terraform/project-id/output.tf
@@ -0,0 +1,10 @@
+output "project-id" {
+  description = "Project ID"
+  value       = google_project.my_project.id
+}
+
+output "project-number" {
+  description = "Project number"
+  value       = google_project.my_project.number
+}
+
---
+++ gcp/terraform/project-id/project.tf
@@ -0,0 +1,4 @@
+resource "google_project" "my_project" {
+  name       = "HQD-GCP"
+  project_id = "hqd-project-id"
+}

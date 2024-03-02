87dc319f26800235036eb1db000c61d20bbd487e
haquocdat543 <wwwdatha543@gmail.com>
2024-03-02 21:17:00 +0700

    Update azure seperation of sql and virtual machine

---
+++ azure/sql/sql_server.tf
@@ -0,0 +1,12 @@
+resource "azurerm_mssql_server" "example" {
+  name                         = "hqd-mssql"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "mradministrator"
+  administrator_login_password = "thisIsDog11"
+
+  tags = {
+    environment = "production"
+  }
+}
--- azure/virtual-machine/vm.tf
+++ azure/virtual-machine/vm.tf
@@ -1,36 +1,3 @@
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East Asia"
-}
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "exampleSubnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "exampleNIC"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "exampleNICConfig"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_virtual_machine" "main" {
   name                  = "test-vm"
   location              = azurerm_resource_group.example.location

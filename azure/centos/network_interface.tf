9f74f1e00a32b89033713b3f8021423d9c8f7fbd
haquocdat543 <wwwdatha543@gmail.com>
2024-03-03 16:27:36 +0700

    Update Azure terraform centos

--- azure/centos/network_interface.tf
+++ azure/centos/network_interface.tf
@@ -1,5 +1,5 @@
-resource "azurerm_network_interface" "example" {
-  name                = "exampleNIC"
+resource "azurerm_network_interface" "loadbalancer" {
+  name                = "loadbalancer"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -7,6 +7,33 @@ resource "azurerm_network_interface" "example" {
     name                          = "exampleNICConfig"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+    public_ip_address_id          = azurerm_public_ip.loadbalancer.id
   }
 }
+
+resource "azurerm_network_interface" "master1" {
+  name                = "master1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "exampleNICConfig"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.master1.id
+  }
+}
+
+resource "azurerm_network_interface" "worker1" {
+  name                = "worker1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "exampleNICConfig"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.worker1.id
+  }
+}
+
--- azure/centos/public_ip.tf
+++ azure/centos/public_ip.tf
@@ -1,5 +1,5 @@
-resource "azurerm_public_ip" "example" {
-  name                    = "test-pip"
+resource "azurerm_public_ip" "loadbalancer" {
+  name                    = "loadbalancer"
   location                = azurerm_resource_group.example.location
   resource_group_name     = azurerm_resource_group.example.name
   allocation_method       = "Dynamic"
@@ -8,3 +8,26 @@ resource "azurerm_public_ip" "example" {
     environment = "test"
   }
 }
+
+resource "azurerm_public_ip" "master1" {
+  name                    = "master1"
+  location                = azurerm_resource_group.example.location
+  resource_group_name     = azurerm_resource_group.example.name
+  allocation_method       = "Dynamic"
+
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_public_ip" "worker1" {
+  name                    = "worker1"
+  location                = azurerm_resource_group.example.location
+  resource_group_name     = azurerm_resource_group.example.name
+  allocation_method       = "Dynamic"
+
+  tags = {
+    environment = "test"
+  }
+}
+

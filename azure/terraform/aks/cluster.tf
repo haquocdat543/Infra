7f19144f54e9bcbf2682dd5c8814b16535743368
haquocdat543 <wwwdatha543@gmail.com>
2024-03-04 09:13:12 +0700

    Update azure terraform AKS

--- azure/terraform/aks/cluster.tf
+++ azure/terraform/aks/cluster.tf
@@ -2,10 +2,11 @@ resource "azurerm_kubernetes_cluster" "example" {
   name                = "flux"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
+  dns_prefix          = "hqd_public_"
+  automatic_channel_upgrade = "stable"
 
   default_node_pool {
-    name       = "default"
+    name       = "first"
     node_count = 3
     vm_size    = "Standard_D2_v2"
   }
@@ -15,6 +16,6 @@ resource "azurerm_kubernetes_cluster" "example" {
   }
 
   tags = {
-    Environment = "Production"
+    Environment = "Development"
   }
 }

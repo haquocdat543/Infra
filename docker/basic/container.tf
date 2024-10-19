eae9d3d7a2770762f20b9ea376eda0758938e9a2
haquocdat543 <wwwdatha543@gmail.com>
2024-10-19 15:33:31 +0700

    docker: basic - add restart for container configuration

--- docker/basic/container.tf
+++ docker/basic/container.tf
@@ -1,6 +1,7 @@
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
+  image   = docker_image.nginx.image_id
+  name    = "tutorial"
+  restart = "always"
   ports {
     internal = 80
     external = 8000
